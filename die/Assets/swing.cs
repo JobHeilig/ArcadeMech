06e1343cf6eb741b960ddf45fa8b321160d71b1a
WaterWalker023 <32755@ma-web.nl>
2022-04-14 12:17:41 +0200

    sound

--- die/Assets/swing.cs
+++ die/Assets/swing.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class swing : MonoBehaviour
 {
     Collision collision;
+    public ParticleSystem particlesystem;
     public bool hit;
     private Animator animator;
+    public float timeStop = 1;
+    public AudioSource bonk;
+    public bool kanHitten;
     // Start is called before the first frame update
     void Start()
     {
+        kanHitten = true;
+        bonk.Stop();
         animator = GetComponent<Animator>();
         hit = false;
+        particlesystem.Stop();
+        
     }
 
     // Update is called once per frame
@@ -21,15 +29,28 @@ public class swing : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.collider.tag);
-        Debug.Log(collision.collider.name);
+        
         if (collision.collider.tag == "zwaard")
         {
             hit = true;
+            //Instantiate(particlesystem);
+            particlesystem.Play();
             animator.SetBool("hit", true);
-            Debug.Log("true");
+            Debug.Log(kanHitten);
+            if (kanHitten == true)
+            {
+                Debug.Log(kanHitten);
+                bonk.Play();
+                kanHitten = false;
+            }
+            
+            StartCoroutine(particlestop(timeStop));
         }
 
     }
-
+    IEnumerator particlestop(float time)
+    {
+        yield return new WaitForSeconds(time);
+        particlesystem.Stop();
+    }
 }
